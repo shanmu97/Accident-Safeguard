eba9d0338b6a019d411c25369b7e1ded6dee748c
SHANMUKHA REDDY VASA <113333841+shanmu97@users.noreply.github.com>
2024-06-13 20:36:50 +0530

    Add files via upload

---
+++ GPS.cpp
@@ -0,0 +1,30 @@
+
+void setup() {
+  Serial.begin(9600);
+  gpsSerial.begin(9600);  // Default baud of NEO-6M GPS module is 9600
+}
+
+void loop() {
+  if (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      if (gps.location.isValid()) {
+        Serial.print(F("- latitude: "));
+        Serial.println(gps.location.lat());
+
+        Serial.print(F("- longitude: "));
+        Serial.println(gps.location.lng());
+
+        Serial.print(F("- altitude: "));
+        if (gps.altitude.isValid())
+          Serial.println(gps.altitude.meters());
+        else
+          Serial.println(F("INVALID"));
+      } else {
+        Serial.println(F("- location: INVALID"));
+      }
+    }
+  }
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
+}
